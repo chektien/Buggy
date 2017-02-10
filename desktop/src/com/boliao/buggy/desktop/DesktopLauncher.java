5712ae953901f4adcdbb35a989a8d95633e6b23e
chektien <chek@gamesstudio.org>
2017-02-11 03:47:31 +0800

    Initial commit

---
+++ desktop/src/com/boliao/buggy/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.boliao.buggy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.boliao.buggy.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
